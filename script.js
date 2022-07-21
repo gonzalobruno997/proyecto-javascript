7ce8ba9e1aad7e9c764327c0487f58fb47d0e010
Gonzalo Bruno <gonzalobruno997@gmail.com>
2022-07-21 15:14:45 -0300

    desafio complementario 1

---
+++ script.js
@@ -0,0 +1,37 @@
+// EJEMPLO 1
+/*let numero = parseInt(prompt("ingrese un numero"));
+for (let i = 1; i <= 5; i++){
+    let resultado = numero + i;
+    console.log(resultado);
+
+    
+}*/ 
+
+//  EJEMPLO 3
+let numero = parseInt(prompt("ingrese un numero"));
+for (let i = 1; i <= numero ; i++) {
+    console.log("hola");
+    
+}
+
+
+
+// for (let i = 1; i <=4 ; i++) {
+//     let valor = parseInt(prompt("ingrese un numero"))
+//     console.log( "el valor ingresado: " + valor)
+    
+// }
+
+// let numero = parseInt(prompt("ingrese un numero"))
+// for (let i = 1; i <= 10; i++) {
+//     console.log(numero * i)
+    
+    
+// }
+
+
+
+
+
+
+    
